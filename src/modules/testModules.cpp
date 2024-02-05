1a55d6b1c9cfb3edda9f770897676a89395d5137
quangle <lenhatquang2512>
2024-02-05 20:55:51 +0700

    add submodules but still not ok, need to write new cmakelists

---
+++ src/modules/testModules.cpp
@@ -0,0 +1,9 @@
+#include "mainClassSample.h"
+
+int main(int argc, char  **argv) {
+    APP::FirstGraphics<int> myObj;
+
+    myObj.process();
+
+    return 0;
+}
--- src/testSoundBasics.cpp
+++ src/testSoundBasics.cpp
@@ -254,7 +254,7 @@ bool init()
 		}
 
 		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		gWindow = SDL_CreateWindow( "Music app FSO", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
 		if( gWindow == NULL )
 		{
 			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
